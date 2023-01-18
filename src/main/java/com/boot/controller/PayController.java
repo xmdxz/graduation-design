29b0ef5944dae3f1415bf67ab5307e3e707d4a61
liyusheng <88344822@qq.com>
2023-01-18 09:42:50 +0800

    feat:新增支付

--- src/main/java/com/boot/controller/PayController.java
+++ src/main/java/com/boot/controller/PayController.java
@@ -4,19 +4,27 @@ import cn.hutool.json.JSONObject;
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.request.AlipayTradeWapPayRequest;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.boot.common.response.Response;
 import com.boot.common.response.ResponseUtil;
+import com.boot.config.AlipayProperty;
 import com.boot.dto.AlipayContent;
 import com.boot.dto.Order;
 import com.boot.enums.OrderStatus;
+import com.boot.exception.ServiceException;
 import com.boot.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @class: PayController
@@ -34,14 +42,8 @@ public class PayController {
     @Resource
     OrderService orderService;
 
-    @PostMapping("submitOrder")
-    @ApiOperation("提交订单")
-    public Response<Order> submitOrder(@RequestBody Order order) {
-        // 创建订单
-        order.setStatus(OrderStatus.WAIT_PAYED);
-        orderService.save(order);
-        return ResponseUtil.success(order);
-    }
+    @Resource
+    AlipayProperty alipayProperty;
 
     @GetMapping("payMoney")
     @ApiOperation("支付宝支付")
@@ -54,6 +56,7 @@ public class PayController {
         bizContent.set("product_code", "QUICK_WAP_WAY");
         bizContent.set("timeout_express", "30m");
         alipayTradeWapPayRequest.setBizContent(bizContent.toString());
+        alipayTradeWapPayRequest.setNotifyUrl(alipayProperty.getNotifyUrl());
         String form = null;
         try {
             form = alipayClient.pageExecute(alipayTradeWapPayRequest).getBody();
@@ -68,7 +71,35 @@ public class PayController {
 
     @PostMapping("notify")
     @ApiOperation("接收支付宝通知")
-    public void receiveMessage() {
-       
+    public void receiveMessage(HttpServletRequest request) {
+        System.out.println("接收到通知");
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        Map<String, String> param = new HashMap<>();
+        for (String name : parameterMap.keySet()) {
+            param.put(name, request.getParameter(name));
+        }
+        LambdaUpdateWrapper<Order> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.set(Order::getStatus, OrderStatus.FINISHED)
+                .set(Order::getAlipayId, param.get("trade_no"))
+                .eq(Order::getId, param.get("out_trade_no"));
+        orderService.update(updateWrapper);
+    }
+
+    @PostMapping("submitOrder")
+    @ApiOperation("提交订单")
+    public Response<Order> submitOrder(@RequestBody Order order) {
+        order.setIsDeleted(0);
+        Order one = orderService.getOne(Wrappers.lambdaQuery(Order.class).eq(Order::getMovieId, order.getMovieId()).eq(Order::getUserId, order.getUserId()));
+        if (!Objects.isNull(one)) {
+            throw new ServiceException("当前订单已创建");
+        }
+        order.setStatus(OrderStatus.WAIT_PAYED);
+        try {
+            orderService.save(order);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServiceException("提交订单异常");
+        }
+        return ResponseUtil.success(order);
     }
 }
