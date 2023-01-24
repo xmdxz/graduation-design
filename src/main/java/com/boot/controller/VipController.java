df2a85984f356aef9e8818495aa7597eeb54e9ce
yangshubo <383723959@qq.com>
2023-01-24 21:30:38 +0800

    会员相关部分内容

--- src/main/java/com/boot/controller/VipController.java
+++ src/main/java/com/boot/controller/VipController.java
@@ -12,6 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
